3b5594339a0e96e13d0280b457b857c0c8865980
kerimov528 <kerimov.anar528@gmail.com>
2023-01-25 04:37:34 -0800

    Fixed

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ const express = require('express');
 const dataSource = require('./src/configs/connect');
 const fs = require('fs');
 const app = express()
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config()
 app.use(express.json())
 
 dataSource
     .initialize()
     .then(() => console.log("Happy"))
     .catch((error) => console.log("Opps", error));
-
 
 const routeHandlers = function (directory, app) {
     fs.readdirSync(directory).forEach(function (folderName) {
--- src/utils/ErrorHandler.js
+++ src/utils/ErrorHandler.js
@@ -1,6 +1,6 @@
 const ErrorHandler = (res,err, next) => {
     const errStatus = err.code || 400;
-    const errMsg = err.message || 'Something went wrong';
+    const errMsg = err.messages || 'Something went wrong';
     res.status(errStatus).json({
         status: errStatus,
         message: errMsg,
